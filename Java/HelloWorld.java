d54133b330b3809b452f8b1602aa44bcaa66ba6f
VijaykrishnaaVV-dev <vijaykrishnaa.v.v@gmail.com>
2025-02-21 00:02:37 +0530

    Just trying again

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Vijay krishnaa");
+        System.out.println("My first");
     }
 }
 
