2fb0227f607dbad980caf8f2bb8cea35dd050dae
VijaykrishnaaVV-dev <vijaykrishnaa.v.v@gmail.com>
2025-02-22 18:38:22 +0530

    started java cool!!

--- Java/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Vijay krishnaa");
-        System.out.println("My first");
-    }
-}
-
