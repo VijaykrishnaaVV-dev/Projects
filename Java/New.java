f25c5f05a38f547328d9003b14a13364e9ce9c9c
VijaykrishnaaVV-dev <vijaykrishnaa.v.v@gmail.com>
2025-02-23 17:33:43 +0530

    My first program in java my own logic :D

--- Java/New.java
+++ Java/New.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class New{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Eneter yout age:");
-        int num = input.nextInt();
-        if (num<18) {
-            System.out.print("Your not eligibal for voting");
-        }else{
-            System.out.println("Your eligibal for voting");
+        System.out.println("Enter start number:");
+        int start = input.nextInt();
+        System.out.println("Enter end number:");
+        int end = input.nextInt();
+        for (int i = start; i <= end; i+=1) {
+            System.out.println(i);
         }
         input.close();
     }
