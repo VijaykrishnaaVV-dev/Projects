4334524d7f12b8e7d25e686b1c264ff8ceda088c
VijaykrishnaaVV-dev <vijaykrishnaa.v.v@gmail.com>
2025-02-23 17:04:26 +0530

    Conditional statments

--- Java/New.java
+++ Java/New.java
@@ -1,10 +1,17 @@
+package Java;
+
 import java.util.Scanner;
 
-public class New {
+public class New{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter your somthing");
-        int rollno = input.nextInt();
-        System.out.println("Your RollNo: "+rollno);
+        System.out.println("Eneter yout age:");
+        int num = input.nextInt();
+        if (num<18) {
+            System.out.print("Your not eligibal for voting");
+        }else{
+            System.out.println("Your eligibal for voting");
+        }
+        input.close();
     }
 }
