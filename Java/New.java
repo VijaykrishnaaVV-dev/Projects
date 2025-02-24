0945f503c4b84e9a7cf069dd3f3d19363ca32c3e
VijaykrishnaaVV-dev <vijaykrishnaa.v.v@gmail.com>
2025-02-24 17:40:37 +0530

    Fibonioc nth number

--- Java/New.java
+++ Java/New.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 
 public class New {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the value:");
-        int start = input.nextInt();
-        int i=start;
-        do{
-            System.out.println(i);
-            i+=1;
-        }while(i<=5);
-        input.close();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();//input nth value to find 
+        int a = 0;//starting number 
+        int b = 1;//next number
+        int count = 2;//count start from 
+        while (count<=n) {//condition to nth number from user input
+            int temp = b;
+            b=a+b;//finding privous value
+            a=temp;//updating the value of a 
+            count++;
+            System.out.println(b);
+        }
+        in.close();
     }
 }
